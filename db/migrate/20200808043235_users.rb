afcac147a755db691b504ec1c186bdbfc0ec2ef1
Tomas Perez <tomasitopg6@gmail.com>
2020-08-08 04:32:51 +0000

    Automatically backed up by Learn

--- db/migrate/20200808043235_users.rb
+++ db/migrate/20200808043235_users.rb
@@ -1,4 +1,10 @@
 class Users < ActiveRecord::Migration[5.1]
   def change
+    def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
+  end
   end
 end
